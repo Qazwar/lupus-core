6759c2b949e548f1b8bfbd7e9b117c3420652f41
David Wolf <d.wolf@live.at>
2014-07-06 01:16:47 +0200

    pragma linking

---
+++ Source/StreamWolfServer/Linker.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#pragma comment(lib, "cryptlib.lib")
+#pragma comment(lib, "libebml.lib")
+#pragma comment(lib, "libmatroska.lib")
--- Source/StreamWolfServer/Main.cpp
+++ Source/StreamWolfServer/Main.cpp
@@ -1,3 +1,5 @@
+#include "Linker.h"
+
 int main(int argc, char** argv)
 {
     return 0;
