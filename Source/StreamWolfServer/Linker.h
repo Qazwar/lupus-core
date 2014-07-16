a07da41d56a424759bfbd441a5c2cf8ba327f88a
David Wolf <d.wolf@live.at>
2014-07-16 18:15:34 +0200

    angelscript hinzugefügt

--- Source/StreamWolfServer/Linker.h
+++ Source/StreamWolfServer/Linker.h
@@ -2,6 +2,7 @@
 
 #ifdef _MSC_VER
 #pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "angelscript.lib")
 #pragma comment(lib, "cryptlib.lib")
 #pragma comment(lib, "libpq.lib")
 #pragma comment(lib, "pugixml.lib")
