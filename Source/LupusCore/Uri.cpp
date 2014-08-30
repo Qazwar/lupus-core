73b5330b4357e9c3d57fa13cb3cb5992ebb6f41b
David Wolf <d.wolf@live.at>
2014-08-30 22:25:13 +0200

    URI ausgebaut

--- Source/LupusCore/Uri.cpp
+++ Source/LupusCore/Uri.cpp
@@ -1,4 +1,7 @@
 ﻿#include "Uri.h"
+#include <cwchar>
+#include <algorithm>
+#include <uriparser/Uri.h>
 
 using namespace std;
 
@@ -6,16 +9,52 @@ namespace Lupus {
     Uri::Uri(const string& uriString) :
         mOriginalString(uriString)
     {
-        int isch = uriString.find("://");
-        int ipor = uriString.find(":", isch);
-        int ipat = uriString.find("/", ipor);
-        int ique = uriString.find("?", ipat);
+        UriParserStateA state;
+        UriUriA uri;
+        int result;
 
-        mSchema = uriString.substr(0, isch);
-        mHost = uriString.substr(isch + 3, ipor);
-        mPort = uriString.substr(ipor + 1, ipat);
-        mPath = uriString.substr(ipat, ique);
-        mQuery = uriString.substr(ique);
+        state.uri = &uri;
+
+        if ((result = uriParseUriA(&state, uriString.c_str())) != URI_SUCCESS) {
+            uriFreeUriMembersA(&uri);
+
+            switch (result) {
+                case URI_ERROR_SYNTAX:
+                    throw format_error(string("Invalid char: '") + *state.errorPos + string("'"));
+                case URI_ERROR_MALLOC:
+                    throw bad_alloc();
+                default:
+                    throw runtime_error("Could not parse given URI");
+            }
+        } else if ((result = uriNormalizeSyntaxA(&uri)) != URI_SUCCESS) {
+            uriFreeUriMembersA(&uri);
+
+            switch (result) {
+                case URI_ERROR_SYNTAX:
+                    throw format_error(string("Invalid char: '") + *state.errorPos + string("'"));
+                case URI_ERROR_MALLOC:
+                    throw bad_alloc();
+                default:
+                    throw runtime_error("Could not parse given URI");
+            }
+        }
+
+        mScheme = string(uri.scheme.first, uri.scheme.afterLast - 1);
+        mUserInfo = string(uri.userInfo.first, uri.userInfo.afterLast - 1);
+        mHostText = string(uri.hostText.first, uri.hostText.afterLast - 1);
+        mQuery = string(uri.query.first, uri.query.afterLast - 1);
+        mFragment = string(uri.fragment.first, uri.fragment.afterLast - 1);
+        mIsAbosultePath = uri.absolutePath != 0;
+
+        for (auto it = uri.pathHead; it != NULL; it = it->next) {
+            mPathHead.push_back(string(it->text.first, it->text.afterLast - 1));
+        }
+
+        for (auto it = uri.pathTail; it != NULL; it = it->next) {
+            mPathTail.push_back(string(it->text.first, it->text.afterLast - 1));
+        }
+
+        uriFreeUriMembersA(&uri);
     }
 
     Uri::Uri(shared_ptr<Uri> baseUri, const string& uriString) :
@@ -28,19 +67,34 @@ namespace Lupus {
     {
     }
 
-    const string& Uri::Host() const
+    Uri::Uri(const wstring& uriString)
     {
-        return mHost;
+        throw runtime_error("not implemented");
     }
 
-    const string& Uri::Port() const
+    Uri::Uri(shared_ptr<Uri> baseUri, const wstring& uriString)
     {
-        return mPort;
+        throw runtime_error("not implemented");
     }
 
-    const string& Uri::Path() const
+    const string& Uri::HostText() const
     {
-        return mPath;
+        return mHostText;
+    }
+
+    const string& Uri::PortText() const
+    {
+        return mPortText;
+    }
+
+    const vector<string>& Uri::PathHead() const
+    {
+        return mPathHead;
+    }
+
+    const vector<string>& Uri::PathTail() const
+    {
+        return mPathTail;
     }
 
     const string& Uri::Query() const
@@ -48,14 +102,9 @@ namespace Lupus {
         return mQuery;
     }
 
-    const string& Uri::Schema() const
+    const string& Uri::Scheme() const
     {
-        return mSchema;
-    }
-
-    string Uri::PathAndQuery() const
-    {
-        return mPath + mQuery;
+        return mScheme;
     }
 
     const string& Uri::OriginalString() const
@@ -63,30 +112,119 @@ namespace Lupus {
         return mOriginalString;
     }
 
-    vector<string> Uri::Segments() const
+    bool Uri::IsAbsolutePath() const
     {
-        vector<string> segments;
-        size_t n = mPath.find("/");
+        return mIsAbosultePath;
+    }
 
-        while (n != mPath.npos) {
-            auto m = mPath.find("/", n);
-            auto sub = m == mPath.npos ? mPath.substr(n + 1) : mPath.substr(n + 1, m - 1);
-            segments.push_back(sub);
-        }
+    string Uri::FullPath() const
+    {
+        string path;
 
-        return segments;
+        for_each(begin(mPathHead), end(mPathHead), [&path](const string& segment) {
+            if (!segment.empty()) {
+                if (path.empty()) {
+                    path += segment;
+                } else {
+                    path += "/" + segment;
+                }
+            }
+        });
+
+        return path;
     }
 
     string Uri::ToString() const
     {
         string uri;
 
-        uri += mSchema.size() > 0 ? mSchema + "://" : "";
-        uri += mHost.size() > 0 ? mHost : "";
-        uri += mPort.size() > 0 ? ":" + mPort : "";
-        uri += mPath.size() > 0 ? mPath : "";
-        uri += mQuery.size() > 0 ? mQuery : "";
+        uri += mScheme.empty() ? "" : mScheme + "://";
+        uri += mUserInfo.empty() ? "" : mUserInfo + "@";
+        uri += mHostText.empty() ? "" : mHostText + "/";
+        uri += mPathHead.empty() ? "" : FullPath();
+        uri += mQuery.empty() ? "" : "?" + mQuery;
+        uri += mFragment.empty() ? "" : "#" + mFragment;
 
         return uri;
+    }
+
+    string Uri::Encode(const string& value)
+    {
+        if (value.empty()) {
+            return "";
+        }
+
+        string result;
+        char* buffer = new char[value.size() * 3 + 1];
+
+        memset(buffer, 0, value.size() * 3 + 1);
+        uriEscapeA(value.c_str(), buffer, true, true);
+        result = buffer;
+        
+        delete buffer;
+        return result;
+    }
+
+    string Uri::Decode(const string& value)
+    {
+        if (value.empty()) {
+            return "";
+        }
+
+        string result;
+        char* buffer = new char[value.size() + 1];
+        
+        memset(buffer, 0, value.size() + 1);
+        memcpy(buffer, value.data(), value.size());
+        uriUnescapeInPlaceExA(buffer, true, URI_BR_DONT_TOUCH);
+        result = buffer;
+
+        delete buffer;
+        return result;
+    }
+
+    bool Uri::CheckSchemeName(const string& value)
+    {
+        return value.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890.+-") != value.npos;
+    }
+
+    wstring Uri::Encode(const wstring& value)
+    {
+        if (value.empty()) {
+            return L"";
+        }
+
+        wstring result;
+        wchar_t* buffer = new wchar_t[value.size() * 3 + 1];
+
+        wmemset(buffer, 0, value.size() * 3 + 1);
+        uriEscapeW(value.c_str(), buffer, true, true);
+        result = buffer;
+
+        delete buffer;
+        return result;
+    }
+
+    wstring Uri::Decode(const wstring& value)
+    {
+        if (value.empty()) {
+            return L"";
+        }
+
+        wstring result;
+        wchar_t* buffer = new wchar_t[value.size() + 1];
+
+        wmemset(buffer, 0, value.size() + 1);
+        wmemcpy(buffer, value.data(), value.size());
+        uriUnescapeInPlaceExW(buffer, true, URI_BR_DONT_TOUCH);
+        result = buffer;
+
+        delete buffer;
+        return result;
+    }
+
+    bool Uri::CheckSchemeName(const wstring& value)
+    {
+        return value.find_first_not_of(L"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890.+-") != value.npos;
     }
 }
--- Source/LupusCore/Uri.h
+++ Source/LupusCore/Uri.h
@@ -3,35 +3,63 @@
 #include "Utility.h"
 #include <vector>
 #include <memory>
+#include <utility>
 #include <boost/noncopyable.hpp>
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4251)
+#endif
 
 namespace Lupus {
     class LUPUS_API Uri : boost::noncopyable
     {
     public:
 
-        Uri(const std::string& uriString) NOEXCEPT;
-        Uri(std::shared_ptr<Uri> baseUri, const std::string& uriString) NOEXCEPT;
-        Uri(std::shared_ptr<Uri> baseUri, std::shared_ptr<Uri> relativeUri) NOEXCEPT;
+        Uri(const std::string& uriString) throw(std::runtime_error, std::bad_alloc, format_error);
+        Uri(const std::wstring& uriString) throw(std::runtime_error, std::bad_alloc, format_error);
+        Uri(std::shared_ptr<Uri> baseUri, const std::string& uriString) throw(std::runtime_error, std::bad_alloc, format_error);
+        Uri(std::shared_ptr<Uri> baseUri, const std::wstring& uriString) throw(std::runtime_error, std::bad_alloc, format_error);
+        Uri(std::shared_ptr<Uri> absoluteUri, std::shared_ptr<Uri> relativeUri) throw(std::runtime_error, std::bad_alloc, format_error);
 
-        const std::string& Host() const NOEXCEPT;
-        const std::string& Port() const NOEXCEPT;
-        const std::string& Path() const NOEXCEPT;
+        const std::string& Scheme() const NOEXCEPT;
+        const std::string& UserInfo() const NOEXCEPT;
+        const std::string& HostText() const NOEXCEPT;
+        const std::string& HostData() const NOEXCEPT;
+        const std::string& PortText() const NOEXCEPT;
+        const std::vector<std::string>& PathHead() const NOEXCEPT;
+        const std::vector<std::string>& PathTail() const NOEXCEPT;
         const std::string& Query() const NOEXCEPT;
-        const std::string& Schema() const NOEXCEPT;
-        std::string PathAndQuery() const NOEXCEPT;
+        const std::string& Fragment() const NOEXCEPT;
         const std::string& OriginalString() const NOEXCEPT;
-        std::vector<std::string> Segments() const NOEXCEPT;
+        bool IsAbsolutePath() const NOEXCEPT;
 
+        std::string FullPath() const NOEXCEPT;
         std::string ToString() const NOEXCEPT;
+
+        static std::string Encode(const std::string&);
+        static std::string Decode(const std::string&);
+        static bool CheckSchemeName(const std::string&);
+        static std::wstring Encode(const std::wstring&);
+        static std::wstring Decode(const std::wstring&);
+        static bool CheckSchemeName(const std::wstring&);
 
     private:
 
-        std::string mHost;
-        std::string mPort;
-        std::string mPath;
+        std::string mScheme;
+        std::string mUserInfo;
+        std::string mHostText;
+        std::string mHostData;
+        std::string mPortText;
+        std::vector<std::string> mPathHead;
+        std::vector<std::string> mPathTail;
         std::string mQuery;
-        std::string mSchema;
+        std::string mFragment;
         std::string mOriginalString;
+        bool mIsAbosultePath;
     };
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
