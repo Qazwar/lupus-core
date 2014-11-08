474abfcebd9559bf744bdace5250ef8763441abf
David Wolf <d.wolf@live.at>
2014-11-08 18:19:04 +0100

    licence

--- Source/BlackWolf.Lupus.Core/Integer.cpp
+++ Source/BlackWolf.Lupus.Core/Integer.cpp
@@ -1,21 +1,20 @@
-﻿/* Copyright � 2014 David Wolf <d.wolf@live.at>
+/**
+ * Copyright (C) 2014 David Wolf <d.wolf@live.at>
  *
- * This file is part of LupusFramwork.
- *
- * LupusFramwork is free software: you can redistribute it and/or modify
+ * This file is part of Lupus.
+ * Lupus is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * LupusFramwork is distributed in the hope that it will be useful,
+ * Lupus is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with LupusFramwork.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Lupus. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "Integer.h"
 #include <cstdio>
 #include <cstring>
--- Source/BlackWolf.Lupus.Utility.GPL/Program.cs
+++ Source/BlackWolf.Lupus.Utility.GPL/Program.cs
@@ -22,7 +22,26 @@ namespace GPL
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lupus. If not, see <http://www.gnu.org/licenses/>.
+ */
+";
 
+            const string gplCopyRight = @"/**
+ * Copyright (C) 2014 David Wolf <d.wolf@live.at>
+ *
+ * This file is part of Lupus.
+ * Lupus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lupus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with Lupus. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -33,8 +52,26 @@ namespace GPL
             foreach (var f in directory.GetFiles("*.h"))
             {
                 var text = File.ReadAllText(f.FullName);
-                if (text.Contains(gpl)) continue;
-                var newText = gpl + text;
+                
+                if (text.Contains(gpl))
+                {
+                    text = text.Remove(text.IndexOf(gpl, System.StringComparison.Ordinal), gpl.Length);
+                }
+
+                var newText = gplCopyRight + text;
+                File.WriteAllText(f.FullName, newText);
+            }
+
+            foreach (var f in directory.GetFiles("*.cpp"))
+            {
+                var text = File.ReadAllText(f.FullName);
+
+                if (text.Contains(gpl))
+                {
+                    text = text.Remove(text.IndexOf(gpl, System.StringComparison.Ordinal), gpl.Length);
+                }
+
+                var newText = gplCopyRight + text;
                 File.WriteAllText(f.FullName, newText);
             }
         }
