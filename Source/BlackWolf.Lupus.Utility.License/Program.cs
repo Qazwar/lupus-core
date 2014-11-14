ea5196b1b8663999f0a6fcfb5ee3dc5c01d48f32
David Wolf <d.wolf@live.at>
2014-11-14 15:47:42 +0100

    Lizenz abgeändert

---
+++ Source/BlackWolf.Lupus.Utility.License/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string oldLicense = @"/**
+ * \author David Wolf
+ *
+ * Copyright (C) 2014 David Wolf <d.wolf@live.at>
+ *
+ * This file is part of Lupus.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the ""Software""), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+";
+
+            const string newLicense = @"/**
+ * Copyright (C) 2014 David Wolf <d.wolf@live.at>
+ *
+ * This file is part of Lupus.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the ""Software""), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+";
+
+            var directory = new DirectoryInfo(".");
+
+            foreach (var f in directory.GetFiles("*.h"))
+            {
+                var text = File.ReadAllText(f.FullName);
+                
+                if (text.Contains(oldLicense))
+                {
+                    text = text.Remove(text.IndexOf(oldLicense, System.StringComparison.Ordinal), oldLicense.Length);
+                }
+
+                var newText = newLicense + text;
+                File.WriteAllText(f.FullName, newText);
+            }
+
+            foreach (var f in directory.GetFiles("*.cpp"))
+            {
+                var text = File.ReadAllText(f.FullName);
+
+                if (text.Contains(oldLicense))
+                {
+                    text = text.Remove(text.IndexOf(oldLicense, System.StringComparison.Ordinal), oldLicense.Length);
+                }
+
+                var newText = newLicense + text;
+                File.WriteAllText(f.FullName, newText);
+            }
+        }
+    }
+}
