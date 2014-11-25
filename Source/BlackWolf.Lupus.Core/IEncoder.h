c8c27762ff11ff6b4918a0031f22babe3f8ea2c7
David Wolf <d.wolf@live.at>
2014-11-25 20:33:48 +0100

    Encoding

---
+++ Source/BlackWolf.Lupus.Core/IEncoder.h
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2014 David Wolf <d.wolf@live.at>
+ *
+ * This file is part of Lupus.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+
+#include "Utility.h"
+
+namespace Lupus {
+    class LUPUSCORE_API IEncoder
+    {
+    public:
+        virtual ~IEncoder() = default;
+
+        virtual String GetString(const Vector<uint8_t>& buffer) const = 0;
+        virtual String GetString(const Vector<uint8_t>& buffer, size_t offset, size_t count) const = 0;
+        virtual Vector<uint8_t> GetBytes(const String& str) const = 0;
+        virtual Vector<uint8_t> GetBytes(const String& str, size_t offset, size_t count) const = 0;
+    };
+}
